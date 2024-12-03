1b7afb0d210074ce04c6c6e4c0048b0fba458a6b
BovineFreedo <frankskorstengaard@gmail.com>
2024-12-03 11:56:02 +0100

    Now the Modal Component works

---
+++ conferencePlanner/conferencePlannerCore/Models/Conference.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace conferencePlannerCore.Models
+{
+    public class Conference
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public DateTime AbstractDeadLine { get; set; }
+        public DateTime ReviewDeadline { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+        public List<string> Category { get; set; }
+        public List<string> ReviewCriteria { get; set; }
+        public Venue venue { get; set; }
+    }
+}
