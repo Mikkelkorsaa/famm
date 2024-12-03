82b9ecba248d57043e9bf14f4d604c0088961336
BovineFreedo <frankskorstengaard@gmail.com>
2024-12-03 13:10:50 +0100

    Create Conference added.

---
+++ conferencePlanner/conferencePlannerApp/Services/Interfaces/IConferenceHandler.cs
@@ -0,0 +1,11 @@
+﻿using conferencePlannerCore.Models;
+
+namespace conferencePlannerApp.Services.Interfaces
+{
+    public interface IConferenceHandler
+    {
+        // A user want to create a conference, the site needs to provide a conference,
+        // The method needs to store the conference.
+        public Task createConference(Conference conference); 
+    }
+}
