dcc3f9ecaa8e26d07d28163bb7d44d65ee601a11
Korsaaaa <mikkelkorsaa@gmail.com>
2024-12-05 15:31:34 +0100

    Refactor controllers to standardize repository variable naming from _repository to _repo

--- conferencePlanner/conferencePlannerApi/Controllers/ConferenceController.cs
+++ conferencePlanner/conferencePlannerApi/Controllers/ConferenceController.cs
@@ -8,23 +8,23 @@ namespace conferencePlannerApi.Controllers
     [Route("api/[controller]")]
     public class ConferenceController : ControllerBase
     {
-        private readonly IConferenceRepo _repository;
+        private readonly IConferenceRepo _repo;
 
-        public ConferenceController(IConferenceRepo repository)
+        public ConferenceController(IConferenceRepo repo)
         {
-            _repository = repository;
+            _repo = repo;
         }
 
         [HttpGet]
         [Route("GetAllConferences")]
         public async Task<IEnumerable<Conference>> GetAllConferences()
-            => await _repository.GetAllAsync();
+            => await _repo.GetAllAsync();
 
         [HttpGet]
         [Route("GetConferenceById/{id}")]
         public async Task<ActionResult<Conference>> GetConferenceById(int id)
         {
-            var conference = await _repository.GetByIdAsync(id);
+            var conference = await _repo.GetByIdAsync(id);
             return conference == null ? NotFound() : conference;
         }
 
@@ -32,7 +32,7 @@ namespace conferencePlannerApi.Controllers
         [Route("CreateConference")]
         public async Task<ActionResult<Conference>> CreateConference(Conference conference)
         {
-            var newConference = await _repository.CreateAsync(conference);
+            var newConference = await _repo.CreateAsync(conference);
             return CreatedAtAction(nameof(GetConferenceById), new { id = newConference.Id }, newConference);
         }
 
@@ -40,13 +40,13 @@ namespace conferencePlannerApi.Controllers
         [Route("UpdateConference")]
         public async Task<ActionResult<Conference>> UpdateConference(Conference conference)
         {
-            var updatedConference = await _repository.UpdateAsync(conference);
+            var updatedConference = await _repo.UpdateAsync(conference);
             return updatedConference == null ? NotFound() : updatedConference;
         }
 
         [HttpDelete]
         [Route("DeleteConference/{id}")]
         public async Task<ActionResult> DeleteConference(int id)
-            => await _repository.DeleteAsync(id) ? NoContent() : NotFound();
+            => await _repo.DeleteAsync(id) ? NoContent() : NotFound();
     }
 }
--- conferencePlanner/conferencePlannerApi/Controllers/UserController.cs
+++ conferencePlanner/conferencePlannerApi/Controllers/UserController.cs
@@ -9,25 +9,25 @@ namespace conferencePlannerApi.Controllers
   [Route("api/[controller]")]
   public class UserController : ControllerBase
   {
-    private readonly IUserRepo _repository;
+    private readonly IUserRepo _repo;
     private readonly IEmailService _emailService;
 
-    public UserController(IUserRepo repository, IEmailService emailService)
+    public UserController(IUserRepo repo, IEmailService emailService)
     {
-      _repository = repository;
+      _repo = repo;
       _emailService = emailService;
     }
 
     [HttpGet]
     [Route("GetAllUsers")]
     public async Task<IEnumerable<User>> GetAllUsers()
-        => await _repository.GetAllAsync();
+        => await _repo.GetAllAsync();
 
     [HttpGet]
     [Route("GetUserById/{id}")]
     public async Task<ActionResult<User>> GetUserById(int id)
     {
-      var user = await _repository.GetByIdAsync(id);
+      var user = await _repo.GetByIdAsync(id);
       return user == null ? NotFound() : user;
     }
 
@@ -35,7 +35,7 @@ namespace conferencePlannerApi.Controllers
     [Route("CreateUser")]
     public async Task<ActionResult<User>> CreateUser(User user)
     {
-      var newUser = await _repository.CreateAsync(user);
+      var newUser = await _repo.CreateAsync(user);
       return CreatedAtAction(nameof(GetUserById), new { id = newUser.Id }, newUser);
     }
 
@@ -43,20 +43,20 @@ namespace conferencePlannerApi.Controllers
     [Route("UpdateUser")]
     public async Task<ActionResult<User>> UpdateUser(User user)
     {
-      var updatedUser = await _repository.UpdateAsync(user);
+      var updatedUser = await _repo.UpdateAsync(user);
       return updatedUser == null ? NotFound() : updatedUser;
     }
 
     [HttpDelete]
     [Route("DeleteUser/{id}")]
     public async Task<ActionResult> DeleteUser(int id)
-        => await _repository.DeleteAsync(id) ? NoContent() : NotFound();
+        => await _repo.DeleteAsync(id) ? NoContent() : NotFound();
 
     [HttpPost]
     [Route("Login")]
     public async Task<ActionResult<User>> Login(LoginModel model)
     {
-      var user = await _repository.GetByEmailAsync(model.Email);
+      var user = await _repo.GetByEmailAsync(model.Email);
       if (user == null || !ValidatePassword(user, model.Password))
       {
         return Unauthorized();
@@ -72,7 +72,7 @@ namespace conferencePlannerApi.Controllers
     [Route("Register")]
     public async Task<IActionResult> Register(RegisterModel request)
     {
-      if (await _repository.GetByEmailAsync(request.Email) != null)
+      if (await _repo.GetByEmailAsync(request.Email) != null)
       {
         return BadRequest("Email already registered");
       }
@@ -84,7 +84,7 @@ namespace conferencePlannerApi.Controllers
         Password = request.Password
       };
 
-      await _repository.CreateAsync(user);
+      await _repo.CreateAsync(user);
 
       return Ok();
     }
@@ -102,7 +102,7 @@ namespace conferencePlannerApi.Controllers
     [Route("GetUserByEmail/{email}")]
     public async Task<ActionResult<User>> GetUserByEmail(string email)
     {
-      var user = await _repository.GetByEmailAsync(email);
+      var user = await _repo.GetByEmailAsync(email);
       return user == null ? NotFound() : user;
     }
 
