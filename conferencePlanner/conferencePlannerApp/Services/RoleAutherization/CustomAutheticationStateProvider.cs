d3d227af1feec7f94548abb60c338fb353aac8e2
Mikkelrv <eaa24mrv@students.eaaa.dk>
2024-12-09 13:17:30 +0100

    Add authentication and authorization to Blazor app
    Updated App.razor to include authentication and authorization by wrapping <Router> in <CascadingAuthenticationState> and replacing <RouteView> with <AuthorizeRouteView>. Added <RedirectToHomePage> component for unauthorized user redirection.
    
    In Program.cs, added namespaces for authentication and authorization, registered CustomAuthenticationStateProvider, and called AddAuthorizationCore.
    
    Updated _Imports.razor to include Microsoft.AspNetCore.Authorization namespace.
    
    Updated conferencePlannerApp.csproj to include package reference for Microsoft.AspNetCore.Components.Authorization.
    
    Added CustomAuthenticationStateProvider class in CustomAuthenticationStateProvider.cs to manage user authentication state using Blazored.LocalStorage.
    
    Added RedirectToHomePage component in RedirectToHomePage.cs to handle unauthorized user redirection.

---
+++ conferencePlanner/conferencePlannerApp/Services/RoleAutherization/CustomAutheticationStateProvider.cs
@@ -0,0 +1,38 @@
+﻿using Blazored.LocalStorage;
+using conferencePlannerCore.Models;
+using Microsoft.AspNetCore.Components.Authorization;
+using System.Security.Claims;
+
+namespace conferencePlannerApp.Services.RoleAutherization
+{
+    public class CustomAuthenticationStateProvider : AuthenticationStateProvider
+    {
+        private readonly ILocalStorageService _localStorage;
+
+        public CustomAuthenticationStateProvider(ILocalStorageService localStorage)
+        {
+            _localStorage = localStorage;
+        }
+
+        public override async Task<AuthenticationState> GetAuthenticationStateAsync()
+        {
+            var user = await _localStorage.GetItemAsync<User>("user");
+
+            if (user == null)
+            {
+                return new AuthenticationState(new ClaimsPrincipal(new ClaimsIdentity()));
+            }
+
+            var claims = new[]
+            {
+            new Claim(ClaimTypes.Email, user.Email),
+            new Claim(ClaimTypes.Role, user.Role.ToString())
+        };
+
+            var identity = new ClaimsIdentity(claims, "custom");
+            var principal = new ClaimsPrincipal(identity);
+
+            return new AuthenticationState(principal);
+        }
+    }
+}
