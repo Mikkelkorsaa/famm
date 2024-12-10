aed0aa5f16de35ae5c23c5725b0245b1f7591cfc
Korsaaaa <mikkelkorsaa@gmail.com>
2024-12-10 19:29:32 +0100

    Refactor services and interfaces: remove unused implementations, update method signatures, and adjust service registrations

--- conferencePlanner/conferencePlannerApp/Services/Interfaces/IConferenceService.cs
+++ conferencePlanner/conferencePlannerApp/Services/Interfaces/IConferenceService.cs
@@ -2,13 +2,13 @@
 
 namespace conferencePlannerApp.Services.Interfaces
 {
-    public interface IConferenceHandler
+    public interface IConferenceService
     {
         // A user want to create a conference, the site needs to provide a conference,
         // The method needs to store the conference.
-        public Task createConference(Conference conference);
+        Task CreateConferenceAsync(Conference conference);
         //A user want to see all conferences that have not yet concluded.
         //The method will return all conferences with a EndDate in the future.
-        public Task<List<Conference>> getActiveConferences(ConferenceFilter filter);
+        Task<IEnumerable<Conference>> GetActiveConferencesAsync(ConferenceFilter filter);
     }
 }
