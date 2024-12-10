94e15c201f3129ffe84b3395818ade2e4f3175b0
Mikkelrv <eaa24mrv@students.eaaa.dk>
2024-12-10 08:43:52 +0100

    injected IAuthService intp CustomAuthenticationStateProvider

--- conferencePlanner/conferencePlannerApp/Services/RoleAutherization/CustomAutheticationStateProvider.cs
+++ conferencePlanner/conferencePlannerApp/Services/RoleAutherization/CustomAutheticationStateProvider.cs
@@ -1,4 +1,5 @@
 ﻿using Blazored.LocalStorage;
+using conferencePlannerApp.Services.Interfaces;
 using conferencePlannerCore.Models;
 using Microsoft.AspNetCore.Components.Authorization;
 using System.Security.Claims;
@@ -8,15 +9,17 @@ namespace conferencePlannerApp.Services.RoleAutherization
     public class CustomAuthenticationStateProvider : AuthenticationStateProvider
     {
         private readonly ILocalStorageService _localStorage;
+        private readonly IAuthService _authService;
 
-        public CustomAuthenticationStateProvider(ILocalStorageService localStorage)
+        public CustomAuthenticationStateProvider(ILocalStorageService localStorage, IAuthService authservice)
         {
             _localStorage = localStorage;
+            _authService = authservice;
         }
 
         public override async Task<AuthenticationState> GetAuthenticationStateAsync()
         {
-            var user = await _localStorage.GetItemAsync<User>("currentUser");
+            var user = await _authService.GetCurrentUser();
 
             if (user == null)
                 return new AuthenticationState(new ClaimsPrincipal(new ClaimsIdentity()));
