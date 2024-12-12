6bf031ff27cb40076c005f90416ca0593056457c
Mikkelrv <eaa24mrv@students.eaaa.dk>
2024-12-12 12:06:33 +0100

    Integrate Radzen components and update UI layout
    Replaced `MongoDBUserRepo` with `LocalUserRepo` in `Program.cs` and added `RadzenComponents` service. Updated `App.razor` to include various Radzen components and replaced `<CascadingAuthenticationState>`. Modified `MainLayout.razor.css` to change `.page` class's `flex-direction` and added a background style. Changed navigation link in `NavMenu.razor` from `createprogram` to `event-planner`. Wrapped `SchedulerPage` in `EventPlanner.razor` with a `div` class `container`. Updated `SchedulerPage.razor` to use `RadzenThreeDayView`. Added `Radzen Blazor` CSS and JS files in `index.html`. Added new `RadzenThreeDayView.cs` file for custom scheduler view. Updated `app.css` with new styles.

--- conferencePlanner/conferencePlannerApp/Program.cs
+++ conferencePlanner/conferencePlannerApp/Program.cs
@@ -13,9 +13,10 @@ var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
+
+builder.Services.AddRadzenComponents();
 builder.Services.AddBlazoredLocalStorage();
 builder.Services.AddAuthorizationCore();
-builder.Services.AddRadzenComponents();
 builder.Services.AddScoped<IAuthService, LocalStorageUserService>();
 builder.Services.AddScoped<IUserService, UserService>();
 builder.Services.AddScoped<IConferenceService, LocalStorageConferenceService>();
