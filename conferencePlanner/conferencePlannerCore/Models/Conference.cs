c191a2485bbbd2fab801b8680fdb120d4b773963
BovineFreedo <frankskorstengaard@gmail.com>
2024-12-15 11:35:37 +0100

    Makeing it again

--- conferencePlanner/conferencePlannerCore/Models/Conference.cs
+++ conferencePlanner/conferencePlannerCore/Models/Conference.cs
@@ -1,6 +1,8 @@
-﻿namespace conferencePlannerCore.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace conferencePlannerCore.Models
 {
-    public record Conference
+    public class Conference
     {
         public int Id { get; set; } = -1;
         public string Name { get; set; } = string.Empty;
@@ -12,6 +14,19 @@
         public List<string> ReviewCriteria { get; set; } = new();
         public Venue Location { get; set; } = new();
         public List<Abstract> Abstracts { get; set; } = new();
-        public List<Review> Reviewers { get; set; } = new();
+        [Required]
+        public List<Reviewer> Reviewers { get; set; } = new();
+
+        public void removeCategory(string category) {
+            if (category != null) {
+                foreach (Reviewer r in Reviewers) {
+                    if(r.Categories.Contains(category)) { 
+                        r.Categories.Remove(category);
+                    }
+                }
+                Category.Remove(category);
+            }
+            return;            
+        }
     }
 }
---
+++ conferencePlanner/conferencePlannerCore/Models/Reviewer.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace conferencePlannerCore.Models
+{
+    public class Reviewer
+    {
+        [Required]
+        public int UserId { get; set; } = -1;
+        [Required]
+        public List<string> Categories { get; set; } = new();
+    }
+}
