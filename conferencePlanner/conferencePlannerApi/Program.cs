ed1f07b039fced67affff8b94d51d4e1a23ca8d3
Korsaaaa <mikkelkorsaa@gmail.com>
2024-12-16 14:56:59 +0100

    Refactor MongoDB repository implementations: switch from IConfiguration to IOptions for MongoDB settings and streamline service registrations

--- conferencePlanner/conferencePlannerApi/Program.cs
+++ conferencePlanner/conferencePlannerApi/Program.cs
@@ -1,10 +1,9 @@
 using conferencePlannerApi.Repositories.Interfaces;
 using conferencePlannerApi.Repositories.LocalImplementations;
-using conferencePlannerApi.Services.Interfaces;
-using conferencePlannerApi.Services.Implementations;
-using conferencePlannerCore.Models;
 using conferencePlannerApi.Repositories.Implementations;
 using conferencePlannerCore.Configurations;
+using conferencePlannerApi.Services.Interfaces;
+using conferencePlannerApi.Services.Implementations;
 using Microsoft.Extensions.FileProviders;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -14,13 +13,26 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 builder.Services.AddSingleton<IUserRepo, MongoDBUserRepo>();
-builder.Services.AddSingleton<IConferenceRepo, LocalConferenceRepo>();
+builder.Services.AddSingleton<IConferenceRepo, MongoDBConferenceRepo>();
 builder.Services.AddSingleton<IAbstractRepo, MongoDBAbstractRepo>();
 builder.Services.AddSingleton<IVenueRepo, LocalVenueRepo>();
-
-builder.Services.Configure<EmailConfiguration>(
-    builder.Configuration.GetSection("EmailConfiguration"));
 builder.Services.AddScoped<IEmailService, EmailService>();
+
+builder.Services.Configure<MongoDBSettings>(options =>
+{
+    options.ConnectionString = "mongodb+srv://mikkelkorsaa:oCkPG6nncfSz1675@conferenceplanner.pufil.mongodb.net/";
+});
+
+builder.Services.Configure<EmailConfiguration>(options =>
+{
+    options.SmtpServer = "smtp.gmail.com";
+    options.SmtpPort = 587;
+    options.SmtpUsername = "danvafamm@gmail.com";
+    options.SmtpPassword = "kenv fqrr cabf qkek";
+    options.FromEmail = "danvafamm@gmail.com";
+    options.FromName = "Danva Famm";
+    options.UseSSL = true;
+});
 
 // Configure the CORS policy
 builder.Services.AddCors(options =>
---
+++ conferencePlanner/conferencePlannerCore/Configurations/MongoDBSettings.cs
@@ -0,0 +1,7 @@
+namespace conferencePlannerCore.Configurations
+{
+  public class MongoDBSettings
+  {
+    public string ConnectionString { get; set; } = string.Empty;
+  }
+}
