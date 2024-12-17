721710fcb5eb13836bf9424d06643194090ef621
Mikkelrv <eaa24mrv@students.eaaa.dk>
2024-12-17 11:33:37 +0100

    Refactor auth logic and update navigation handling
    Simplified authorization by removing `AuthorizeView` components and role-based attributes, opting for direct role checks within components. Updated `AdminDashboardComponent` to include navigation event handlers. Refactored `DashboardPage` to use a switch statement for role-based rendering and fetch user info from `AuthService`. Removed custom authentication state provider and related policies, indicating a shift in authentication mechanism. Updated `_Imports.razor` and `conferencePlannerApp.csproj` to reflect these changes.

--- conferencePlanner/conferencePlannerApp/Services/LocalImplementations/LocalStorageStorageAuthService .cs
+++ conferencePlanner/conferencePlannerApp/Services/LocalImplementations/LocalStorageStorageAuthService .cs
@@ -1,9 +1,6 @@
 using conferencePlannerApp.Services.Interfaces;
 using conferencePlannerCore.Models;
 using Blazored.LocalStorage;
-using Microsoft.AspNetCore.Components.Authorization;
-using conferencePlannerApp.Services.RoleAutherization;
-using Microsoft.AspNetCore.Components;
 
 namespace conferencePlannerApp.Services.LocalImplementations
 {
