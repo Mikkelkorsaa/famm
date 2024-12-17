b7a8b7c9764944e821743cd304a40daffa721a8b
Frank Skorstengaard <1612-au651342@user.noreply.gitlab.au.dk>
2024-12-17 07:04:38 +0100

    changeing computer again not working commit

--- conferencePlanner/conferencePlannerApp/Services/Interfaces/IUserService.cs
+++ conferencePlanner/conferencePlannerApp/Services/Interfaces/IUserService.cs
@@ -2,20 +2,36 @@ using conferencePlannerCore.Models;
 
 namespace conferencePlannerApp.Services.Interfaces
 {
-  public interface IUserService
-  {
-    // Output: List of all users
-    Task<List<User>> GetAllUsersAsync();
+    public interface IUserService
+    {
+        // Output: List of all users
+        Task<List<User>> GetAllUsersAsync();
 
-    // Input: User object
-    // Manipulation: Saves to database
-    // Output: User object with generated ID
-    Task UpdateUserAsync(User user);
-    /// <summary>
-    /// Get the current userId in local storage
-    /// </summary>
-    
-    /// <returns>UserId</returns>
-    Task<int?> GetCurrentUserIdAsync();
-  }
+        // Input: User object
+        // Manipulation: Saves to database
+        // Output: User object with generated ID
+        Task UpdateUserAsync(User user);
+        /// <summary>
+        /// Get the current userId in local storage
+        /// </summary>
+
+        /// <returns>UserId</returns>
+        Task<int?> GetCurrentUserIdAsync();
+        /// <summary>
+        /// Input: A filter containing, a search query string, int length of the list called shown and a int how many should be skipped, 
+        /// so you chose the maximum amount shown.
+        /// </summary>
+        /// <param name="filter"></param>
+        /// <returns>A list of Users, length can be set by the filter</returns>
+        Task<List<User>> GetUsersBySearchOrFilter(UserFilter filter);
+        /// <summary>
+        /// A filter containing, a search query string, int length of the list called shown and a int how many should be skipped, 
+        /// so you chose the maximum amount shown.
+        /// </summary>
+        /// <param name="filter"></param>
+        /// <returns>how many hits does this search have</returns>
+        Task<int> GetUsersBySearchOrFilterHits(UserFilter filter);
+
+
+    }
 }
---
+++ conferencePlanner/conferencePlannerCore/Models/UserFilter.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace conferencePlannerCore.Models
+{
+    public class UserFilter
+    {
+        public string Query { get; set; } = string.Empty;
+        public int numberOfUsersShown {get; set;} = 0;
+        public int numberOfUsersSkipped { get; set; } = 0;
+    }
+}
