ac41ddcff1679325d6630ffbb886c7d4f867a04c
Korsaaaa <mikkelkorsaa@gmail.com>
2024-12-20 09:32:39 +0100

    Final code for examination

--- conferencePlanner/conferencePlannerApi/Program.cs
+++ conferencePlanner/conferencePlannerApi/Program.cs
@@ -1,9 +1,7 @@
 using conferencePlannerApi.Repositories.Interfaces; 
 using conferencePlannerApi.Repositories.LocalImplementations; 
 using conferencePlannerApi.Repositories.Implementations; 
-using conferencePlannerCore.Configurations; 
-using conferencePlannerApi.Services.Interfaces;
-using conferencePlannerApi.Services.Implementations; 
+using conferencePlannerCore.Configurations;
 using Microsoft.Extensions.FileProviders; 
 
 // Create a builder for the web application
@@ -19,24 +17,11 @@ builder.Services.AddSingleton<IUserRepo, MongoDBUserRepo>();
 builder.Services.AddSingleton<IConferenceRepo, MongoDBConferenceRepo>(); 
 builder.Services.AddSingleton<IAbstractRepo, MongoDBAbstractRepo>(); 
 builder.Services.AddSingleton<IVenueRepo, MongoDBVenueRepo>(); 
-builder.Services.AddScoped<IEmailService, EmailService>(); 
 
 // Configure MongoDB settings
 builder.Services.Configure<MongoDBSettings>(options =>
 {
     options.ConnectionString = "mongodb+srv://mikkelkorsaa:oCkPG6nncfSz1675@conferenceplanner.pufil.mongodb.net/"; 
-});
-
-// Configure email settings
-builder.Services.Configure<EmailConfiguration>(options =>
-{
-    options.SmtpServer = "smtp.gmail.com"; // Set SMTP server address
-    options.SmtpPort = 587; // Set SMTP server port
-    options.SmtpUsername = "danvafamm@gmail.com"; // Set email username
-    options.SmtpPassword = "kenv fqrr cabf qkek"; // Set email password
-    options.FromEmail = "danvafamm@gmail.com"; // Set sender email address
-    options.FromName = "Danva Famm"; // Set sender name
-    options.UseSSL = true; // Enable SSL for email communication
 });
 
 // Configure the CORS policy to allow all origins, methods, and headers
